91591a0113e2531526f37cf0855800765cd1afd4
Daniel Grote <danielgrote@Daniels-MacBook-Pro-5.local>
2015-01-20 02:51:13 -0600

    Added new graph library

--- Code/current/www/index.php
+++ Code/current/www/index.php
@@ -6,86 +6,90 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<base href="bryalle.duckdns.org">
 		<title>Home</title>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+		<script src="javascript/highcharts.js"></script>
+		<script src="javascript/exporting.js"></script>
+		<script src="javascript/draggable-points.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<?php
-			// show top nav bar and zipcode input
-			include $_SERVER['DOCUMENT_ROOT']."/includes/navbar.html";
-			include $_SERVER['DOCUMENT_ROOT']."/includes/enterzip.html";
+	<?php
+	
+		// show top nav bar and zipcode input
+		include $_SERVER['DOCUMENT_ROOT']."/includes/navbar.html";
+		include $_SERVER['DOCUMENT_ROOT']."/includes/enterzip.html";
 			
-			// require libraries
-			require_once($_SERVER['DOCUMENT_ROOT'].'/../libraries/nusoap/nusoap.php');
-			require_once($_SERVER['DOCUMENT_ROOT'].'/../libraries/simple-nws/SimpleNWS.php');
-			require_once($_SERVER['DOCUMENT_ROOT'].'/php/MyMain.php');
-			use SimpleNWS\SimpleNWS;
+		// require libraries
+		require_once($_SERVER['DOCUMENT_ROOT'].'/../libraries/nusoap/nusoap.php');
+		require_once($_SERVER['DOCUMENT_ROOT'].'/../libraries/simple-nws/SimpleNWS.php');
+		require_once($_SERVER['DOCUMENT_ROOT'].'/php/MyMain.php');
+		use SimpleNWS\SimpleNWS;
 			
-			// if enter button was pressed
-			if(isset($_POST['btn_enter']) && $_POST['tb_zip'] != ""){
+		// if enter button was pressed
+		if(isset($_POST['btn_enter']) && $_POST['tb_zip'] != ""){
 				
-				$ziplist = array($_POST['tb_zip']);
-				$isMetric = isset($_POST['cb_metric']);
-				$customCTemp = $_POST['tb_ctemp'] != "";
-				
-				// create soap client
-				$soapclient = new nusoap_client('http://www.weather.gov/forecasts/xml/SOAP_server/ndfdXMLserver.php?wsdl');
 
-				// get lon and lat from zip code
-				$LatLonList = $soapclient->call('LatLonListZipCode',$ziplist,
+			$ziplist = array($_POST['tb_zip']);
+			$zipcode = $ziplist[0];
+				
+			$isMetric = isset($_POST['cb_metric']);
+			$customCTemp = $_POST['tb_ctemp'] != "";
+				
+			// create soap client
+			$soapclient = new nusoap_client('http://www.weather.gov/forecasts/xml/SOAP_server/ndfdXMLserver.php?wsdl');
+
+			// get lon and lat from zip code
+			$LatLonList = $soapclient->call('LatLonListZipCode',$ziplist,
 										   'uri:DWMLgen',
 										   'uri:DWMLgen/LatLonListZipCode');
 
-				$latlon = new SimpleXMLElement($LatLonList);
-				$latlon = explode(',', $latlon->latLonList[0]);
+			$latlon = new SimpleXMLElement($LatLonList);
+			$latlon = explode(',', $latlon->latLonList[0]);
 				
-				$simpleNWS = new SimpleNWS(floatval($latlon[0]), floatval($latlon[1]));
-				$main = new Main();
+			$simpleNWS = new SimpleNWS(floatval($latlon[0]), floatval($latlon[1]));
+			$main = new Main();
 				
-				try{
-					// request a forecast (getCurrentConditions(), getForecastForToday() or getForecastForWeek())
-					$forecast = $simpleNWS->getForecastForWeek();
-					$time_layouts = $forecast->getTimeLayouts();
-					$hourly_temp = $forecast->getHourlyRecordedTemperature();
-					$hourly_humidity = $forecast->getHourlyHumidity();
-					$hourly_windspeed = $forecast->getHourlyWindSpeed();
-					$hourly_cloudcover = $forecast->getHourlyCloudCover();
+			try{
+				// request a forecast (getCurrentConditions(), getForecastForToday() or getForecastForWeek())
+				$forecast = $simpleNWS->getForecastForWeek();
+				$time_layouts = $forecast->getTimeLayouts();
+				$hourly_temp = $forecast->getHourlyRecordedTemperature();
+				$hourly_humidity = $forecast->getHourlyHumidity();
+				$hourly_windspeed = $forecast->getHourlyWindSpeed();
+				$hourly_cloudcover = $forecast->getHourlyCloudCover();
 					
-					
-					$i = 0;
-					foreach($time_layouts as $key => $value){
-						if($i == 2){
-							foreach($time_layouts[$key] as $time){
-								$aTemp = $hourly_temp[$time];
-								$cTemp = $hourly_temp[$time];
-								$hum = $hourly_humidity[$time];
-								$wSpd = $hourly_windspeed[$time];
-								$cCover = $hourly_cloudcover[$time];
-								if($customCTemp){
-									$cTemp = $_POST['tb_ctemp'];
+				$i = 0;
+				foreach($time_layouts as $key => $value){
+					if($i == 2){
+						foreach($time_layouts[$key] as $time){
+							$aTemp = $hourly_temp[$time];
+							$cTemp = $hourly_temp[$time];
+							$hum = $hourly_humidity[$time];
+							$wSpd = $hourly_windspeed[$time];
+							$cCover = $hourly_cloudcover[$time];
+							if($customCTemp){
+								$cTemp = $_POST['tb_ctemp'];
 								}
-								$main->calcEvap($isMetric, $customCTemp, $time, $aTemp, $hum, $wSpd, $cTemp, $cCover);
-							}
+							$main->calcEvap($isMetric, $customCTemp, $time, $aTemp, $hum, $wSpd, $cTemp, $cCover);
 						}
-						$i++;
 					}
-					// draw graph
-					include $_SERVER['DOCUMENT_ROOT']."/includes/graph.php";
+					$i++;
 				}
-				
-				
-				catch (\Exception $error){
-					if( $error->getMessage() == "Invalid latitude. Allowed values are between 20.19 and 50.11"){
-						echo "<font color='red'>Please enter a valid zip code</font>";
-					}
-					
-				}
-				
+				// draw graph
+				include $_SERVER['DOCUMENT_ROOT']."/includes/graph.php";
 			}
-		?>
-		
-		<script src="javascript/Chart.js"></script>
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="bootstrap/js/bootstrap.min.js"></script>
-	</body>
+				
+				
+			catch (\Exception $error){
+				if( $error->getMessage() == "Invalid latitude. Allowed values are between 20.19 and 50.11"){
+					echo "<font color='red'>Please enter a valid zip code</font>";
+				}
+					
+			}
+				
+		}
+	?>
+
+
+</body>
 </html>
