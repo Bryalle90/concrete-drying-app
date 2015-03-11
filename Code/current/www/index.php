8e9c4290baf91554bf0ae1ba8f835242f0cdedac
Bryan Allen <bryanpallen90@gmail.com>
2015-03-10 20:52:47 -0500

    changed form submit to get instead of post

--- Code/current/www/index.php
+++ Code/current/www/index.php
@@ -15,11 +15,15 @@
 	</head>
 	<body>
 		<?php
+            $ZIPPATTERN = "/\b\d{5}\b/"; // us zip code regex
+            
 			// start session
 			session_start();
-			
+            
 			// if user is logged in
-			$loggedin = isset($_SESSION['user']);
+            $loggedin = 0;
+            if (isset($_SESSION['user']))
+                $loggedin = 1;
 		
 			// show top nav bar and zipcode input
 			include $_SERVER['DOCUMENT_ROOT']."/includes/navbar.html";
@@ -29,76 +33,76 @@
 			require_once($_SERVER['DOCUMENT_ROOT'].'/../libraries/nusoap/nusoap.php');
 			require_once($_SERVER['DOCUMENT_ROOT'].'/../libraries/simple-nws/SimpleNWS.php');
 				
-			// if enter button was pressed
-			if(isset($_POST['btn_enter']) && $_POST['tb_zip'] != ""){
-				$ziplist = array($_POST['tb_zip']);
-				$zipcode = $ziplist[0];
-                
-                $isMetric = 0;
-                if (isset($_POST['cb_metric']))
-                    $isMetric = 1;
-                $customCTemp = 0;
-                if (isset($_POST['tb_ctemp']) && $_POST['tb_ctemp'] != NULL)
-                    $customCTemp = 1;
+			// if the zip code is valid
+			if(isset($_GET['tb_zip']))
+                if (preg_match($ZIPPATTERN, $_GET['tb_zip'])){
+                    $ziplist = array($_GET['tb_zip']);
                     
-				// create soap client
-				$soapclient = new nusoap_client('http://www.weather.gov/forecasts/xml/SOAP_server/ndfdXMLserver.php?wsdl');
+                    $isMetric = 0;
+                    if (isset($_GET['cb_metric']))
+                        $isMetric = 1;
+                    $customCTemp = 0;
+                    if (isset($_GET['tb_ctemp']) && $_GET['tb_ctemp'] != NULL)
+                        $customCTemp = 1;
+                        
+                    // create soap client
+                    $soapclient = new nusoap_client('http://www.weather.gov/forecasts/xml/SOAP_server/ndfdXMLserver.php?wsdl');
 
-				// get lon and lat from zip code
-				$LatLonList = $soapclient->call('LatLonListZipCode',$ziplist,
-											   'uri:DWMLgen',
-											   'uri:DWMLgen/LatLonListZipCode');
+                    // get lon and lat from zip code
+                    $LatLonList = $soapclient->call('LatLonListZipCode',$ziplist,
+                                                   'uri:DWMLgen',
+                                                   'uri:DWMLgen/LatLonListZipCode');
 
-				$latlon = new SimpleXMLElement($LatLonList);
-				$latlon = explode(',', $latlon->latLonList[0]);
-				$simpleNWS = new SimpleNWS(floatval($latlon[0]), floatval($latlon[1]));
-				
-				?>
-				<script>
-					main = new Main(<?=$zipcode?>, <?=$isMetric?>);
-				</script>
-				<?php
-				try{
-					// request a forecast (getCurrentConditions(), getForecastForToday() or getForecastForWeek())
-					$forecast = $simpleNWS->getForecastForWeek();
-					$time_layouts = $forecast->getTimeLayouts();
-					$hourly_temp = $forecast->getHourlyRecordedTemperature();
-					$hourly_humidity = $forecast->getHourlyHumidity();
-					$hourly_windspeed = $forecast->getHourlyWindSpeed();
-					$hourly_cloudcover = $forecast->getHourlyCloudCover();
-						
-					$i = 0;
-					foreach($time_layouts as $key => $value){
-						if($i == 2){
-							foreach($time_layouts[$key] as $time){
-								$aTemp = $hourly_temp[$time];
-								$cTemp = $hourly_temp[$time];
-								$hum = $hourly_humidity[$time];
-								$wSpd = $hourly_windspeed[$time];
-								$cCover = $hourly_cloudcover[$time];
-								if($customCTemp){
-									$cTemp = $_POST['tb_ctemp'];
-								}
-								?>
-									<script>
-										main.fillArrays(<?=$customCTemp?>, <?=$aTemp?>, <?php echo json_encode($time) ?>, <?=$hum?>, <?=$wSpd?>, <?=$cTemp?>, <?=$cCover?>);
-									</script>
-								<?php
-							}
-						}
-						$i++;
-					}
-					// draw graph
-					include $_SERVER['DOCUMENT_ROOT']."/includes/graph.html";
-				}
-				catch (\Exception $error){
-					if( $error->getMessage() == "Invalid latitude. Allowed values are between 20.19 and 50.11"){
-						echo "<font color='red'>Please enter a valid zip code</font>";
-					}
-						
-				}
-			}
+                    $latlon = new SimpleXMLElement($LatLonList);
+                    $latlon = explode(',', $latlon->latLonList[0]);
+                    $simpleNWS = new SimpleNWS(floatval($latlon[0]), floatval($latlon[1]));
+                    
+                    ?>
+                    <script>main = new Main(<?=$ziplist[0]?>, <?=$isMetric?>);</script>
+                    <?php
+                    try{
+                        // request a forecast (getCurrentConditions(), getForecastForToday() or getForecastForWeek())
+                        $forecast = $simpleNWS->getForecastForWeek();
+                        $time_layouts = $forecast->getTimeLayouts();
+                        $hourly_temp = $forecast->getHourlyRecordedTemperature();
+                        $hourly_humidity = $forecast->getHourlyHumidity();
+                        $hourly_windspeed = $forecast->getHourlyWindSpeed();
+                        $hourly_cloudcover = $forecast->getHourlyCloudCover();
+                            
+                        $i = 0;
+                        foreach($time_layouts as $key => $value){
+                            if($i == 2){
+                                foreach($time_layouts[$key] as $time){
+                                    $aTemp = $hourly_temp[$time];
+                                    $cTemp = $hourly_temp[$time];
+                                    $hum = $hourly_humidity[$time];
+                                    $wSpd = $hourly_windspeed[$time];
+                                    $cCover = $hourly_cloudcover[$time];
+                                    if($customCTemp){
+                                        $cTemp = $_GET['tb_ctemp'];
+                                    }
+                                    ?>
+                                        <script>
+                                            main.fillArrays(<?=$customCTemp?>, <?=$aTemp?>, <?php echo json_encode($time) ?>, <?=$hum?>, <?=$wSpd?>, <?=$cTemp?>, <?=$cCover?>);
+                                        </script>
+                                    <?php
+                                }
+                            }
+                            $i++;
+                        }
+                        // draw graph
+                        include $_SERVER['DOCUMENT_ROOT']."/includes/graph.html";
+                    }
+                    catch (\Exception $error){
+                        if( $error->getMessage() == "Invalid coordinates."){
+                            echo '<div class="alert alert-danger" role="alert">Please enter a valid zip code</div>';
+                        }
+                            
+                    }
+                } else {
+                    echo '<div class="alert alert-danger" role="alert">Please enter a valid zip code</div>';
+                }
 		?>
         
-</body>
+    </body>
 </html>
