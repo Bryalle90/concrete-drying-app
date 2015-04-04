757f8f983cd6808dac5f14318fa63ba62c09939b
Bryan Allen <bryanpallen90@gmail.com>
2015-04-04 18:30:32 -0500

    number of projects in session

--- Code/current/www/php/login.php
+++ Code/current/www/php/login.php
@@ -1,5 +1,6 @@
 <?php
-    include $_SERVER['DOCUMENT_ROOT']."/../classes/DbUser.php";
+    require_once($_SERVER['DOCUMENT_ROOT']."/../classes/DbUser.php");
+    require_once($_SERVER['DOCUMENT_ROOT'].'/../classes/DbProject.php');
 	// if user tried to log in
 	if(isset($_POST['btn_signin']) && $_POST['tb_email'] != "" && $_POST['tb_pass'] != ""){
 		$userdb = new DbUser();
@@ -10,17 +11,19 @@
 			$_SESSION = array();
 			session_destroy();
 			session_start();
+            $projectdb = new DbProject();
             
 			$_SESSION['id'] = $userID;
 			$_SESSION['user'] = $userdb->getName($userID);
 			$_SESSION['email'] = $userdb->getEmail($userID);
 			$_SESSION['admin'] = $userdb->isUserAdmin($userID);
+			$_SESSION['numProjects'] = count($projectdb->getProjects($userID));
         }
 	}
 	if(!isset($_SESSION['user'])){
         header("Location: /../login_page.php");
 		//include $_SERVER['DOCUMENT_ROOT']."/includes/login_page.html";
 	} else {
-		header("Location: /../index.php");
+		header("Location: /../projects.php");
 	}
 ?>
