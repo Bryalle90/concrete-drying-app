7e9e0c87d18b58cae05f0bdc32beb965e62cae98
Zachary D. Smith <zachsmi@siue.edu>
2015-04-04 22:11:06 -0500

    Add last 3 database interaction classes and tables creation statements

--- Code/current/classes/DbChangeInStateNotification.php
+++ Code/current/classes/DbChangeInStateNotification.php
@@ -1,35 +1,62 @@
-//ChangeInStateNotification.php
-//Class used to interact with the cisNotification table in our database.
-//by: zach smith
-//last edited: 2/2/15
+<?php
 
+//DbChangeInStateNotification.php
+//Class used to interact with the changeinstatenotifications table in our database.
+//by: zach smith
+//last edited: 4/4/15
+
+require $_SERVER['DOCUMENT_ROOT']."/../libraries/password-compat/lib/password.php";
 
 class DbChangeInStateNotification {
-
-	//These 3 variables will need to be changed to specific case
-	private $hostname, $username, $password, $dbhandle;
-
-	public function _construct($hostname, $username, $password){
-		$this->hostname = $hostname;
-		$this->username = $username;
-		$this->password = $password;
-	}
+	
+	private $dbhandle;
+	
+	public function __construct(){
+        $this->connectdb();
+    }
 
 	//connect to the database
 	public function connectdb(){			
-		$dbhandle = mysql_connect($hostname, $username, $password)
-			OR die("Unable to connect to MySQL");
-		//output to confirm connection
-		echo "Connected to MySQL<br>";
-		
-		$selected = mysql_select_db("Account", $dbhandle)
-			OR die("Could not select");
+		$this->dbhandle = mysql_connect('localhost', 'root', '');
+					
+		$selected = mysql_select_db("Account", $this->dbhandle);
 	}
 
-	
+	//inserts a new notification to the table
+	public function addNotification($projectID){
+		$projectID = mysql_real_escape_string($projectID);
+		$sql = "INSERT INTO changeInStateNotification (projectID)
+		VALUES ('$projectID')";
+		mysql_query($sql);		
+	}
+
+	//delete notification from table
+	public function deleteNotification($futureID){		
+		$futureID = mysql_real_escape_string($futureID);
+		$sql = "DELETE FROM changeInStateNotification WHERE changeInStateNotificationID = '$changeInStateNotificationID'";
+		mysql_query($sql);
+	}	
+
+	//gets the changeInStateNotificationID by using the project id
+	public function getchangeInStateNotificationID($projectID){
+		$projectID = mysql_real_escape_string($projectID);
+		$sql = "SELECT changeInStateNotificationID FROM changeInStateNotification WHERE projectID = '$projectID'";
+		$result = mysql_query($sql);
+        if (!$result || !mysql_num_rows($result))
+            return(Null);
+		$result = mysql_result($result, 0);
+		return $result;
+	}
+
 	//close the connection
-	public function _destruct(){
+	public function disconnectdb(){
 		mysql_close($dbhandle);
 	}
 
+	public function _destruct(){
+		$this->disconnectdb();
+	}
+
 }
+
+?>
