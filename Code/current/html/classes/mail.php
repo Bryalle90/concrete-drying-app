1019481653f03ce9bec6bbd60f9ac7129b24bf1e
Bryan Allen <bryanpallen90@gmail.com>
2015-04-21 17:37:47 -0500

    updating mail class from ITS server

--- Code/current/html/classes/mail.php
+++ Code/current/html/classes/mail.php
@@ -18,37 +18,39 @@ $forgotMessage ="Hello,\r\n\r\nYou have requested to reset your password. Please
 mail($emailacc, $subject, $forgotMessage, $headers);
 }
 
-public function AddUserToProject($username, $url) 
+public function AddUserToProject($emailacc, $projectName) 
 {
 $subject = 'You have been added to a Project!';
-$addMessage = "Hello,\r\n\r\nYou have been added to a project. Please click the link below and log in to view the project.\r\n\r\n" . $url . "\r\n\r\nIf you don't have an account you will have to create one.\r\n\r\nThanks!";
+$url = 'https://plasticcracks.siue.edu/projects.php';
+$addMessage = "Hello,\r\n\r\nYou have been added to a project. Please click the link below and navigate to ".$projectName." where you can join the project.\r\n\r\n" . $url . "\r\n\r\nIf you don't have an account you will have to create one with this e-mail address.\r\n\r\nThanks!";
 $headers = 'From: plasticcracks@siue.edu' . "\r\n";
-mail($username, $subject, $addMessage, $headers);
+mail($emailacc, $subject, $addMessage, $headers);
 }
 
-public function EmailGraph($username, $url, $zipcode, $name, $usrmsg) 
+public function EmailGraph($emailacc, $url, $zipcode, $name, $usrmsg) 
 {
 $subject = 'Plastic Shinkage Cracks';
 $headers = 'From: plasticcracks@siue.edu' . "\r\n";
-$msg = "Hello,\r\n\r\n".$name." has sent you the probablility that concrete will form plastic shinkage cracks for ".$zipcode.".\r\n\r\nUser added message: ".$usrmsg."\r\n\r\nClick the link below to see the predictions.\r\n\r\n" . $url . "\r\n\r\nThanks!";
-mail($username, $subject, $msg, $headers);
+$msg = "Hello,\r\n\r\n".$name." has sent you the probablility that concrete will form plastic shinkage cracks for zip code ".$zipcode.".\r\n\r\n".$name." said: ".$usrmsg."\r\n\r\nClick the link below to see the predictions.\r\n\r\n" . $url . "\r\n\r\nThanks!";
+mail($emailacc, $subject, $msg, $headers);
 } 
 
-public function FutureNotif($username, $url, $zipcode, $date) 
+public function FutureNotif($emailacc, $projectName, $zipcode, $date) 
 {
 $subject = "Your predictions are ready!";
 $headers = 'From: plasticcracks@siue.edu' . "\r\n";
-$futureMSG = "Hello,\r\n\r\nYour concrete plastic shrinkage crack predictions are ready for ".$zipcode." on ".$date." to view. \r\n\r\nClick the link below to view the predictions.\r\n\r\n".$url."\r\n\r\nThanks!";
-mail($username, $subject, $futureMSG, $headers);
+$url = 'https://plasticcracks.siue.edu/projects.php';
+$futureMSG = "Hello,\r\n\r\nYour predictions are ready for ".$projectName." at zip code ".$zipcode.".\r\n\r\nClick the link below and select ".$projectName." to view the predictions.\r\n\r\n".$url."\r\n\r\nThanks!";
+mail($emailacc, $subject, $futureMSG, $headers);
 }
 
-public function ChangeInRiskNotif($username, $zipcode, $time, $date, $risk) 
+public function ChangeInRiskNotif($emailacc, $projectName, $zipcode, $date, $oldRisk, $newRisk) 
 {
 $url = 'https://plasticcracks.siue.edu/projects.php';
 $headers = 'From: plasticcracks@siue.edu' . "\r\n";
-$subject = "PROJECT Notiofcation";
-$notifMSG = "Hello,\r\n\r\nThe predicted risk of plastic shinkage cracks prediction changed for ".$zipcode." at ".$time." and now has a ".$risk." risk".$date.".\r\n\r\nClick the link below to view the changes.\r\n\r\n".$url."\r\n\r\nThanks!";
-mail($username, $subject, $notifMSG, $headers);
+$subject = "Change In Risk Notification";
+$notifMSG = "Hello,\r\n\r\nThe predicted risk for ".$projectName." on ".$date." has changed from ".$oldRisk." risk to ".$newRisk." risk.\r\n\r\nClick the link below and select view on ".$projectName." to view the changes.\r\n\r\n".$url."\r\n\r\nThanks!";
+mail($emailacc, $subject, $notifMSG, $headers);
 }
 
 }
