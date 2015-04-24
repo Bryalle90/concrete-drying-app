a8da112757c9e3ec2db56731de30300cf5b34457
Daniel Grote <danielgrote@Daniels-MacBook-Pro.local>
2015-04-24 00:44:01 -0500

    Added get notifications

--- Code/current/html/php/getNotif.php
+++ Code/current/html/php/getNotif.php
@@ -4,8 +4,57 @@ include($_SERVER['DOCUMENT_ROOT'].'/classes/DbChangeInStateNotification.php');
 $pid = $_POST['pID'];
 $seriesdb= new DbSeries();
 $risk = new DbChangeInStateNotification();
+$numbOfNotif = $risk->getNumbOfNotificationID($pid);
 
-//print_r($risk->getNotif($pid));
-print_r($risk->getchangeInStateNotificationID($pid));
+
+
+
+if ($numbOfNotif > 0)
+{
+    $idArr = $risk->getchangeInStateNotificationID($pid);
+    $seriesID = array();
+    $timeArr = array();
+    $currentZoneArr = array();
+    $notifyZoneArr = array();
+    
+    for($i = 0; $i < $numbOfNotif; $i++)
+    {
+        
+        $timeArr[$i] = $risk->getTime($idArr[$i]);
+        $seriesID[$i] = $risk->getSeries($idArr[$i]);
+        $currentZoneArr[$i] = $risk->getCurrentZone($idArr[$i]);
+        $notifyZoneArr[$i] = $risk->getNotifyZone($idArr[$i]);
+        
+        
+        if ($seriesdb->getIsOriginal($seriesID[$i]) == 'y')
+        {
+            $seriesID[$i] = 'original';
+        }
+    }
+    
+   // $arr = $risk->getNotif($pid);
+   echo json_encode( array(
+    'changeInStateID' => $idArr,
+    'sid' => $seriesID,
+    'time' => $timeArr,
+    'currentZone' => $currentZoneArr,
+    'notifyZone' => $notifyZoneArr
+    ));
+}
+
+else
+{
+    echo 'None';
+}
+
+
+/*
+echo json_encode( array(
+    'seriesID' => $idArr,
+    'time' => $timeArr,
+    'currentZone' => $currentZoneArr,
+    'notifyZone' => $notifyZoneArr
+    ));
+    */
 
 ?>
--- Code/current/html/php/removeSeries.php
+++ Code/current/html/php/removeSeries.php
@@ -2,6 +2,9 @@
 include($_SERVER['DOCUMENT_ROOT'].'/classes/DbSeries.php');
 $seriesdb= new DbSeries();
 $seriesID = $_POST['sID'];
+//TODO Delete notifcations on that series
+
 $seriesdb->deleteSeries($seriesID);
 echo('Done');
+
 ?>
