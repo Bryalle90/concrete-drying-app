f1e4dd50c1426e9668a37a09985cd1d3937711ae
Bryan Allen <bryanpallen90@gmail.com>
2015-04-24 19:53:26 -0500

    add to log when creating risk notification

--- Code/current/html/php/addNotif.php
+++ Code/current/html/php/addNotif.php
@@ -1,18 +1,39 @@
 <?php 
 include($_SERVER['DOCUMENT_ROOT'].'/classes/DbSeries.php');
 include($_SERVER['DOCUMENT_ROOT'].'/classes/DbChangeInStateNotification.php');
+$risk = new DbChangeInStateNotification();
+$seriesdb= new DbSeries();
 $seriesID = $_POST['sID'];
 $pid = $_POST['pID'];
 $time = $_POST['time'];
+$currentZone = $_POST['zCurrent'];
+$wantedZone = $_POST['zWanted'];
 
-if ($seriesID == 'orginal')
+
+if ($seriesID == 'original')
 {
-    $seriesdb= new DbSeries();
+    
     $seriesID = $seriesdb->getOriginalSeriesID($pid);
 }
 
-$risk = new DbChangeInStateNotification();
-$risk->addNotification($pid, $seriesID, $time, $_POST['zCurrent'], $_POST["zWanted"]);
-echo 'done';
+if ($risk->checkNotification($pid, $seriesID, $time, $currentZone , $wantedZone) > 0)
+{
+    echo 'Notification has already been created';
+}
+else
+{
+	$risk->addNotification($pid, $seriesID, $time, $currentZone , $wantedZone);
+	
+	require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbLog.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbProject.php');
+	$logger = new DbLog();
+	$projectdb = new DbProject();
+	
+	$zip = $projectdb->getZipcode($pid);
+	$logger->addRiskCreated($zip, $time);
+	
+    echo 'success';
+}
+
 
 ?>
